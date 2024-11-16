8fb20097af6dbdc02769cd0a6871e1ff13a2140e
datnt16904 <tuandathb102@gmail.com>
2024-11-16 18:29:38 +0700

    fix dashboard

--- env.php
+++ env.php
@@ -2,6 +2,6 @@
 //Lấy thư mục gốc của file
 const ROOT_DIR = __DIR__ . "/";
 //Lưu đường dẫn website
-const ROOT_URL = "http://localhost/test/";
+const ROOT_URL = "http://localhost/da1_g10store/";
 //Lưu đường dẫn admin
 const ADMIN_URL = ROOT_URL . "admin/";
